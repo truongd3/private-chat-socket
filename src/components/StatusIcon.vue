176f188dd7be71e066d34ee399cc99da3b0358f1
Truong Dang <94695575+2dtkingslayer@users.noreply.github.com>
2023-12-01 00:53:17 -0500

    upload src folder

---
+++ src/components/StatusIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <i class="icon" :class="{ connected: connected }"></i>
+</template>
+
+<script>
+export default {
+  name: "StatusIcon",
+  props: {
+    connected: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+.icon {
+    height: 8px;
+    width: 8px;
+    border-radius: 50%;
+    display: inline-block;
+    background-color: #5f6368;
+    margin-right: 6px;
+}
+
+.icon.connected {
+    background-color: #34a853;
+}
+</style>
